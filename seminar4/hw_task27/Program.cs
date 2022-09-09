248abbcbbf8dfc6ce8e30aeec736035bdc01d10d
Druce <raa-07@mail.ru>
2022-09-09 23:47:07 +0300

    hw_task27 + hw_task29

--- seminar4/hw_task27/Program.cs
+++ seminar4/hw_task27/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 27. Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int d = 0;
+while (n > 0)
+{
+    d = n % 10;
+    res = res + d;
+    n = n / 10;
+}
+Console.WriteLine(res);
--- seminar4/hw_task29/Program.cs
+++ seminar4/hw_task29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 29. Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+Console.WriteLine("Введите длину массива(по заданию длина массива 8 символов):");
+int l = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+
+if (l == 8)
+{
+Console.WriteLine("Введите поочередно элементы массива:");
+int[] array = new int[l];
+for (n = 0; n < l; n++)
+{
+    array[n] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Введенный массив:");
+Console.WriteLine(string.Join(" ", array));
+}
+else
+{
+    Console.WriteLine("Написано же 8!!!");
+}
