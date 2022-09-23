8341876300c22c786696ada41c3ce49dd43cc0f7
Druce <raa-07@mail.ru>
2022-09-23 22:44:43 +0300

    seminar8

---
+++ seminar8/task55/Program.cs
@@ -0,0 +1,38 @@
+﻿// 55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void GetMatrix(int[,] matrix)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(-10, 11);
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReverseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            Console.Write($"{matrix[j, i]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[i, j];
+Console.WriteLine("Полученая случайная матрица:");
+GetMatrix(matrix);
+Console.WriteLine("Поменяные местами первая и последняя строки:");
+ReverseMatrix(matrix);
