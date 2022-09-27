eb8a0c34b58c58f69b31bede977c1b954f259fab
Druce <raa-07@mail.ru>
2022-09-28 00:28:02 +0300

    fix68

--- seminar9/hw_task68/Program.cs
+++ seminar9/hw_task68/Program.cs
@@ -12,8 +12,7 @@ int Akkerman(int m, int n)
 {
     if (m == 0) return n += 1;
     else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
-    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
-    return n;
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
 }
 
 int m = ReadNumber("Введите M: ");
